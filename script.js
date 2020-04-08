fc421ec6c2db9545253856020f071ccbad154251
Majd Sufian <majdnewsufian@gmail.com>
2020-04-08 15:44:39 +0200

    optimize my js code

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-/**
- * Weather App
- * DONE: Complete getWeatherData() to return json response Promise
- * DONE: Complete searchCity() to get user input and get data using getWeatherData()
- * DONE: Complete showWeatherData() to set the data in the the html file from response
- */
-
 // API_KEY for maps api
- // city name
- // we need weather.main
- // we need main.temp + temp_min + temp_max
+//  city name
+//  we need weather.main
+//  we need main.temp + temp_min + temp_max
 
 getWeatherData = (city) => {
   const URL =  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=metric`);
-  //HINT: Use template literals to create a url with input and an API key
   return URL.then((response) => {
     return response.json();
   })
@@ -20,7 +12,6 @@ getWeatherData = (city) => {
 
 searchCity = () => {
   const city = document.getElementById('city-input').value;
-  // CODE GOES HERE
   getWeatherData(city)
   .then((response) => {
     showWeatherData(response)
